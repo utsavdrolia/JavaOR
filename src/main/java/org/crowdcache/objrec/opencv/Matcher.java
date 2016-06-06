98b090e480268c6ab7fdd3c848606ee182e1ec1e
Utsav Drolia <utsavdrolia@gmail.com>
2016-06-06 16:57:44 -0400

    Made Matcher abstract and added functions that are common across matcher implementations.

--- src/main/java/org/crowdcache/objrec/opencv/Matcher.java
+++ src/main/java/org/crowdcache/objrec/opencv/Matcher.java
@@ -1,9 +1,15 @@
 package org.crowdcache.objrec.opencv;
 
+import org.opencv.core.MatOfDMatch;
+import org.opencv.features2d.DMatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by utsav on 2/7/16.
  */
-public interface Matcher
+public abstract class Matcher
 {
     /**
      * Match the 2 images and return a match score
@@ -11,6 +17,30 @@ public interface Matcher
      * @param sceneImage
      * @return
      */
-    Double match(KeypointDescList dbImage, KeypointDescList sceneImage);
-    Matcher newMatcher();
+    public abstract Double match(KeypointDescList dbImage, KeypointDescList sceneImage);
+    public abstract Matcher newMatcher();
+
+    /**
+     * Ratio test for descriptor matches
+     * @param matches
+     * @return
+     */
+    public List<DMatch> ratioTest(List<MatOfDMatch> matches)
+    {
+        List<DMatch> good_matches = new ArrayList<>();
+
+        // Ratio test
+        for(MatOfDMatch dmatch: matches)
+        {
+            DMatch[] arr = dmatch.toArray();
+            DMatch m = arr[0];
+            DMatch n = arr[1];
+            // Release of the MatOfDMatch
+            dmatch.release();
+            if(m.distance < 0.7*n.distance)
+                good_matches.add(m);
+        }
+
+        return good_matches;
+    }
 }
