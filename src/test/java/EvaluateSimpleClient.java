bf5498e18e70633ef447fbb6cd249db82285edef
Utsav Drolia <utsavdrolia@gmail.com>
2016-07-26 10:54:18 -0400

    Classes to run and evaluate different types of clients. Server should already be running.

---
+++ src/test/java/EvaluateSimpleClient.java
@@ -0,0 +1,33 @@
+import org.crowdcache.objrec.opencv.FeatureExtractor;
+import org.crowdcache.objrec.opencv.Matcher;
+import org.crowdcache.objrec.rpc.CachedObjRecClient;
+import org.crowdcache.objrec.rpc.ObjRecClient;
+import org.opencv.core.Core;
+
+import java.io.IOException;
+
+/**
+ * Created by utsav on 2/5/16.
+ */
+public class EvaluateSimpleClient
+{
+    public static void main(String args[]) throws IOException, InterruptedException
+    {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        if (args.length == 3)
+        {
+            String queryList = args[0];
+            String resultspath = args[1];
+            String serverAdd = args[2];
+
+            ObjRecClient objRecClient = new ObjRecClient(serverAdd);
+
+            Util.evaluate(objRecClient, queryList, resultspath);
+        }
+        else
+        {
+            System.out.println("3 Args required, only provided " + args.length);
+        }
+        System.exit(0);
+    }
+}
